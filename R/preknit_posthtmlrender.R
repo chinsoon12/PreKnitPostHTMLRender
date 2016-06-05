d880dba94f0cbb777ec27f13a3c870a0beaff04f
chinsoon12 <chinsoon12@hotmail.com>
2016-06-05 13:57:45 +0800

    fixed examples

--- R/preknit_posthtmlrender.R
+++ R/preknit_posthtmlrender.R
@@ -164,13 +164,11 @@ post_html_render_proc <- function(inhtml, outhtml=NULL) {
 #' First Pre-knitting Processing, then knitr::knit, then rmarkdown::render,
 #' then Post HTML-rendering Processing
 #'
-#' In pre-knitting processing, function takes in a Rmd file, evaluates code chunks
-#' which are marked as between adjacent lines of @@@s, and then
+#' 1) In pre-knitting processing, function takes in a Rmd file, evaluates code chunks
+#' which are marked as between adjacent lines of @@@@s, and then
 #' replace these code chunks with evaluated output captured using capture.output function.
-#'
-#' Function then calls knitr::knit followed by rmarkdown::render.
-#'
-#' After which, in post HTML-rendering processing, function evaluates code chunks
+#' 2) Function then calls knitr::knit followed by rmarkdown::render.
+#' 3) After which, in post HTML-rendering processing, function evaluates code chunks
 #' in between `%%% MY_CODE_HERE `, and then replace these code chunks with evaluated output
 #' captured using capture.output function. Typically, we want to insert an external HTML file
 #' and we can use `%%% writeLines(readLines("MY_EXTERNAL_HTML_FILE"))` within the Rmd file.
@@ -181,7 +179,8 @@ post_html_render_proc <- function(inhtml, outhtml=NULL) {
 #'
 #' @param outhtml - output html file
 #'
-#' @return The output html file will have the evaluated @@@ and/or `%%% ` code chunks
+#' @return The output html file will have the codes between @@@@@@
+#' and/or \`\%\%\% \` (where \` is a backtick) code chunks evaluated
 #'
 #' @import knitr
 #'
@@ -192,36 +191,41 @@ post_html_render_proc <- function(inhtml, outhtml=NULL) {
 #' @export
 #'
 #' @examples
-#' samplermd <- tempfile("test", getwd(), ".Rmd")
-#' addhtml <- normalizePath(tempfile("additional", getwd(), ".html"),'/',FALSE)
+#' \donttest{
+#' #pandoc.exe is required to run this code
+#' samplermd <- tempfile('test', getwd(), '.Rmd')
+#' addhtml <- 'test__test.html'
+#'
 #' #generate the test Rmd file
-#' cat(paste0('---
-#' title: "Example Usage"
-#' output: html_document
-#' ---
-#'
-#' This document is used for various similar reports.
-#'
-#' @@@
-#' cat(paste("#Dynamic Header1", rnorm(1)))
-#' @@@
-#'
-#' `%%% writeLines(readLines("',addhtml,'"))`
-#'
-#' @@@
-#' cat(paste("##Dynamic Header2", rnorm(1)))
-#' @@@
-#'
-#' Some content is invariant across different reports.\n'), file=samplermd)
+#' writeLines(c('---',
+#' 'title: "Example Usage"',
+#' 'output: html_document',
+#' '---',
+#' '',
+#' 'This document is used for various similar reports.',
+#' '',
+#' '@@@@@@',
+#' "cat(paste('#Dynamic Header1', rnorm(1)))",
+#' '@@@@@@',
+#' '',
+#' '`%%% writeLines(readLines("test__test.html"))`',
+#' '',
+#' '@@@@@@',
+#' 'cat(paste("##Dynamic Header2", rnorm(1)))',
+#' '@@@@@@',
+#' '',
+#' 'Some content is invariant across different reports.'), samplermd)
 #'
 #' #generate test html file
-#' cat('<ul>
-#' <li>Item 1</li>
-#' <li>Item 2</li>
-#' <li>Item 3</li>
-#' </ul>\n', file=addhtml)
+#' writeLines(c('<ul>',
+#' '<li>Item 1</li>',
+#' '<li>Item 2</li>',
+#' '<li>Item 3</li>',
+#' '</ul>'), addhtml)
 #'
+#' #Pre-knit processing and post HTML render processing
 #' preknit_knit_render_postrender(samplermd)
+#' }
 #'
 preknit_knit_render_postrender <- function(pRmdfile, outhtml=NULL) {
     bn <- file_path_sans_ext(basename(pRmdfile))
